4e7edfee5f5eea4c8dd6c546cd685bd63f87edc2
aitzeng <aitzeng@gmail.com>
2023-04-11 18:59:46 -0700

    Partially complete

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,21 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = ( {videos, clicked} ) => {
+
+  return (
+
+    <div className="video-list">
+      {videos.map(video => <VideoListEntry video = {video} clicked = {clicked} key = {video.id.videoId}/>)}
+    </div>
+    // <div className="video-list">
+    //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    // </div>
+  );
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
